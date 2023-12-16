3236b5746456ce6a052d9b16a387b548e1009d7c
sushithjhenkar <sushithjhenkar@gmail.com>
2023-12-16 17:22:52 -0500

    Added Some more info

--- run.py
+++ run.py
@@ -1,27 +1,28 @@
+from flask import Flask, render_template, request
+
 import requests
+
+app = Flask(__name__)
 
 API_KEY = '9c1df31231e04ea57f0f732b33e8f072'
 
-while True:
-    city_name = input("Enter city name: ")
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        city_name = request.form['city']
+        weather_data = get_weather_data(city_name)
+        return render_template('index.html', city=city_name, weather_data=weather_data)
+    return render_template('index.html', city=None, weather_data=None)
 
+def get_weather_data(city_name):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}'
 
     response = requests.get(url)
 
     if response.status_code == 200:
         weather_data = response.json()
-
-        # Extracting relevant information
-        weather_description = weather_data['weather'][0]['description']
-        temperature_kelvin = weather_data['main']['temp']
-
-        # Converting temperature from Kelvin to Celsius
-        temperature_celsius = temperature_kelvin - 273.15
-
-        print(f"Weather in {city_name}: {weather_description}")
-        print(f"Temperature: {temperature_celsius:.2f}°C")
     else:
-        print(f"Error {response.status_code}: Unable to retrieve weather data for {city_name}. Please check the city name.")
+        return f"Error {response.status_code}: Unable to retrieve weather data for {city_name}. Please check the city name."
 
-print('sushith')
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True, host='127.0.0.1', port=5000)
