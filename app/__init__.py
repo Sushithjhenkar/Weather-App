cc8f36e6308d9baf4aa3f4f339d6cbedefe5ea2e
sushithjhenkar <sushithjhenkar@gmail.com>
2023-12-17 13:49:36 -0500

    Added time

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,9 @@ def index():
     if request.method == 'POST':
         city_name = request.form['city']
         weather_data = get_weather_data(city_name)
-        return render_template('index.html', city=city_name, weather_data=weather_data)
-    return render_template('index.html', city=None, weather_data=None)
+        time_data = get_time_data(city_name)
+        return render_template('index.html', city=city_name, weather_data=weather_data, time_data=time_data)
+    return render_template('index.html', city=None, weather_data=None, time_data=None)
 
 def get_weather_data(city_name):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}'
@@ -22,9 +23,15 @@ def get_weather_data(city_name):
         weather_description = weather_data['weather'][0]['description']
         temperature_kelvin = weather_data['main']['temp']
         temperature_celsius = temperature_kelvin - 273.15
-        # weather_icon = weather_data['weather']['0']['icon']
-        # return {'description': weather_description, 'temperature': f"{temperature_celsius:.2f}°C", 'icon': {weather_icon}}
         return weather_data
+    else:
+        return None
+def get_time_data(city_name):
+    time_url = 'https://api.api-ninjas.com/v1/worldtime?city={}'.format(city_name)
+    time_response = requests.get(time_url, headers={'X-Api-Key': 'ax637K4C0PDTdRQtSobr9g==3IC0TkpMYuu6GIB4'})
+    if time_response.status_code == 200:
+        time_data = time_response.json()
+        return time_data
     else:
         return None
 
