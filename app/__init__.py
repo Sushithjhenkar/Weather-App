3dbeac5899c02df5ea10ff4e8dbc1c4363d15bfa
sushithjhenkar <sushithjhenkar@gmail.com>
2023-12-17 19:52:44 -0500

    Pre-Final Commit

--- app/__init__.py
+++ app/__init__.py
@@ -10,8 +10,9 @@ def index():
     if request.method == 'POST':
         city_name = request.form['city']
         weather_data = get_weather_data(city_name)
-        return render_template('index.html', city=city_name, weather_data=weather_data)
-    return render_template('index.html', city=None, weather_data=None)
+        time_data = get_time_data(city_name)
+        return render_template('index.html', city=city_name, weather_data=weather_data, time_data=time_data)
+    return render_template('index.html', city=None, weather_data=None, time_data=None)
 
 def get_weather_data(city_name):
     url = f'http://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}'
@@ -30,6 +31,13 @@ def get_weather_data(city_name):
         return weather_data
     else:
         return None
-
+def get_time_data(city_name):
+    time_url = 'https://api.api-ninjas.com/v1/worldtime?city={}'.format(city_name)
+    time_response = requests.get(time_url, headers={'X-Api-Key': 'ax637K4C0PDTdRQtSobr9g==3IC0TkpMYuu6GIB4'})
+    if time_response.status_code == 200:
+        time_data = time_response.json()
+        return time_data
+    else:
+        return None
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True, host='127.0.0.1', port=5000)
